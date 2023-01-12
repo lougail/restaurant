b69731b7b31e6b099a60cbadc891d6dd09103bd2
lougail <louis.gaillard94@gmail.com>
2023-01-12 16:56:59 +0100

    Suppression fichier perdu

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeeders;
 
+use Faker;
 use App\Models\Restaurant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,8 +16,28 @@ class RestaurantSeeder extends Seeder
      */
     public function run ()
     {
-        $restaurant = new Restaurant();
-        $restaurant->setCle('adresse');
-        $restaurant->setValeur('5 bd Louis XIV', '59000', 'Lille');
+        $faker = Faker\Factory::create('fr_FR');
+
+        $restaurantDatas = [
+            [
+                'cle' => 'adresse',
+                'valeur' => $faker->address(),
+            ],
+            [
+                'cle' => 'tel',
+                'valeur' => $faker->phoneNumber(),
+            ],
+        ];
+
+        foreach ($restaurantDatas as $restaurantData) {
+            // création d'une nouvelle catégorie
+            $restaurant = new Restaurant();
+            // affectation d'un nom
+            $restaurant->cle = $restaurantData['cle'];
+            // affectation d'une description
+            $restaurant->valeur = $restaurantData['valeur'];
+            // sauvegarde dans la BDD
+            $restaurant->save();
+        } 
     }
 }
